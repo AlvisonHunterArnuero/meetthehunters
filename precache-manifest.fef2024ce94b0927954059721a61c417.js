f917f0f9fae6ff03cc53858b1d0485e9faf84fa3
Alvison Hunter <alvisonhunter@Jimmys-MacBook-Pro.local>
2019-10-25 23:15:06 -0600

    Updates

--- precache-manifest.fef2024ce94b0927954059721a61c417.js
+++ precache-manifest.fef2024ce94b0927954059721a61c417.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9811b6bc6a5c0b314c240b4f653a4d06",
+    "revision": "858cef3a6ee235e2013a297d8173a393",
     "url": "/meetthehunters/index.html"
   },
   {
-    "revision": "45bdce77c8091e2c10f9",
-    "url": "/meetthehunters/static/css/main.2e3ec8da.chunk.css"
+    "revision": "4d34e778aba3b3e85297",
+    "url": "/meetthehunters/static/css/main.d7e350c4.chunk.css"
   },
   {
     "revision": "160db71e3ba2750abc5b",
     "url": "/meetthehunters/static/js/2.c40437aa.chunk.js"
   },
   {
-    "revision": "45bdce77c8091e2c10f9",
-    "url": "/meetthehunters/static/js/main.dbec32fd.chunk.js"
+    "revision": "4d34e778aba3b3e85297",
+    "url": "/meetthehunters/static/js/main.cdf8eee3.chunk.js"
   },
   {
     "revision": "13eb27a6866ea15759cb",
